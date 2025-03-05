8ffb00335efe7a30febf44d750e0b26417d7847a
RobertsKSmits <90193490+Nevelin-W@users.noreply.github.com>
2025-03-05 17:35:01 +0200

    Update HomeServlet.java

--- src/main/java/com/example/HomeServlet.java
+++ src/main/java/com/example/HomeServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet("/")
 public class HomeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
